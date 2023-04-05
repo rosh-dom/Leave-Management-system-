cbdc72ad385c5bfcb6dbd1da5981889a15e87a2f
rosh-dom <roshanchristuraj07@gmail.com>
2023-04-05 21:50:38 +0530

    Initial commit

---
+++ leave_management.py
@@ -0,0 +1,13 @@
+from config import*
+from functions import *
+# imprt mysql.connector
+
+# cur.close()
+#add_val(4,"Vacation", "7th April", "8th April", "Pending")
+# add_val(7,"Vacation", "7th April", "9th April","pending")
+# add_val(8,"Vacation", "7th April", "10th April","pending")
+# add_val(9,"Vacation", "7th April", "11th April","pending")
+approve_leave(200)
+reject_leave(200)
+display_table()
+#cur = conn.cursor()
